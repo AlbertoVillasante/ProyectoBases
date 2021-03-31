9d64f035f8be21f21fa3fe49674e3c9da9f04cb6
hugo <hugo@hugo>
2021-03-31 11:20:43 +0200

    cambio properties

--- src/baseDatos/FachadaBaseDatos.java
+++ src/baseDatos/FachadaBaseDatos.java
@@ -24,7 +24,9 @@ public class FachadaBaseDatos {
         FileInputStream arqConfiguracion;
 
         try {
-            arqConfiguracion = new FileInputStream("baseDatos.properties");
+            //arqConfiguracion = new FileInputStream("baseDatos.properties");
+            arqConfiguracion = new FileInputStream("basesDatos.properties");
+
             configuracion.load(arqConfiguracion);
             arqConfiguracion.close();
 
